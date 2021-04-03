361a3f2d913e3765974833437f9b789afeab0fe8
Mozes721 <Richard.Taujenis@gmail.com>
2021-04-03 11:52:11 +0200

    Update blackjack_pygame.py

--- blackjack_pygame.py
+++ blackjack_pygame.py
@@ -2,7 +2,7 @@ import pygame as pygame
 from blackjack_deck import *
 from constants import *
 import sys
-
+import time
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -66,107 +66,121 @@ def button(msg, x, y, w, h, ic, ac, action=None):
 
 
 class Play:
-    in_play = True
-    if in_play:
-        def __init__(self):
-            self.deck = Deck()
-            self.dealer = Hand()
-            self.player = Hand()
-            self.deck.shuffle()
+    def __init__(self):
+        self.deck = Deck()
+        self.dealer = Hand()
+        self.player = Hand()
+        self.deck.shuffle()
         
-        def blackjack(self):
+    def blackjack(self):
 
-            self.dealer.calc_hand()
-            self.player.calc_hand()
-            print("DEALER")
-            print(self.dealer.value)
-            print("PLAYER")
-            print(self.player.value)
-            if self.player.value == 21 and self.dealer.value == 21:
-                black_jack("Both with BlackJack!", 500, 250, grey)
-            elif self.player.value == 21:
-                black_jack("You  BlackJack!", 500, 250, green)
-            elif self.dealer.value == 21:
-                show_dealer_card = pygame.image.load('img/' + self.dealer.card_img[1] + '.png').convert()
-                gameDisplay.blit(show_dealer_card, (550, 200))
-                black_jack("Dealer has BlackJack!", 500, 250, red)
-            
-            self.player.value = 0
-            self.dealer.value = 0
-
-        def deal(self):
-            for i in range(2):
-                self.dealer.add_card(self.deck.deal())
-                self.player.add_card(self.deck.deal())
-            self.dealer.display_cards()
-            self.player.display_cards()
-            self.player_card = 1
-            dealer_card = pygame.image.load('img/' + self.dealer.card_img[0] + '.png').convert()
-            dealer_card_2 = pygame.image.load('img/back.png').convert()
-            
-            player_card = pygame.image.load('img/' + self.player.card_img[0] + '.png').convert()
-            player_card_2 = pygame.image.load('img/' + self.player.card_img[1] + '.png').convert()
-
-        
-            game_texts("Dealer's hand is:", 500, 150)
-
-            gameDisplay.blit(dealer_card, (400, 200))
-            gameDisplay.blit(dealer_card_2, (550, 200))
-
-            game_texts("Your's hand is:", 500, 400)
-        
-            gameDisplay.blit(player_card, (300, 450))
-            gameDisplay.blit(player_card_2, (410, 450))
-            self.blackjack()
-            
-            
-            
-        def hit(self):
-            self.player.add_card(self.deck.deal())
-            self.blackjack()
-            self.player_card += 1
-            if self.player_card == 2:
-                self.player.calc_hand()
-                self.player.display_cards()
-                player_card_3 = pygame.image.load('img/' + self.player.card_img[2] + '.png').convert()
-                gameDisplay.blit(player_card_3, (520, 450))
-
-                if self.player.value > 21:
-                    game_finish("You Busted!", 500, 250, red)
-            
-                
-            if self.player_card == 3:
-                self.player.calc_hand()
-                self.player.display_cards()
-                player_card_4 = pygame.image.load('img/' + self.player.card_img[3] + '.png').convert()
-                gameDisplay.blit(player_card_4, (630, 450))
-                
-                if self.player.value > 21:
-                    game_finish("You Busted!", 500, 250, red)
-            
-            self.player.value = 0
-
-            if self.player_card > 4:
-                sys.exit()
-            
-            
-        def stand(self):
+        self.dealer.calc_hand()
+        self.player.calc_hand()
+        print("DEALER")
+        print(self.dealer.value)
+        print("PLAYER")
+        print(self.player.value)
+        if self.player.value == 21 and self.dealer.value == 21:
+            black_jack("Both with BlackJack!", 500, 250, grey)
+        elif self.player.value == 21:
+            black_jack("You  BlackJack!", 500, 250, green)
+        elif self.dealer.value == 21:
             show_dealer_card = pygame.image.load('img/' + self.dealer.card_img[1] + '.png').convert()
             gameDisplay.blit(show_dealer_card, (550, 200))
-            self.blackjack()
-            self.dealer.calc_hand()
-            self.player.calc_hand()
-            if self.player.value > self.dealer.value:
-                game_finish("You Won!", 500, 250, green)
-            elif self.player.value < self.dealer.value:
-                game_finish("Dealer Wins!", 500, 250, red)
-            else:
-                game_finish("It's a Tie!", 500, 250, grey)
+            black_jack("Dealer has BlackJack!", 500, 250, red)
+            
+        self.player.value = 0
+        self.dealer.value = 0
+
+    def deal(self):
+        for i in range(2):
+            self.dealer.add_card(self.deck.deal())
+            self.player.add_card(self.deck.deal())
+        self.dealer.display_cards()
+        self.player.display_cards()
+        self.player_card = 1
+        dealer_card = pygame.image.load('img/' + self.dealer.card_img[0] + '.png').convert()
+        dealer_card_2 = pygame.image.load('img/back.png').convert()
+            
+        player_card = pygame.image.load('img/' + self.player.card_img[0] + '.png').convert()
+        player_card_2 = pygame.image.load('img/' + self.player.card_img[1] + '.png').convert()
+
         
-        def exit(self):
+        game_texts("Dealer's hand is:", 500, 150)
+
+        gameDisplay.blit(dealer_card, (400, 200))
+        gameDisplay.blit(dealer_card_2, (550, 200))
+
+        game_texts("Your's hand is:", 500, 400)
+        
+        gameDisplay.blit(player_card, (300, 450))
+        gameDisplay.blit(player_card_2, (410, 450))
+        self.blackjack()
+            
+            
+            
+    def hit(self):
+        self.player.add_card(self.deck.deal())
+        self.blackjack()
+        self.player_card += 1
+        if self.player_card == 2:
+            self.player.calc_hand()
+            self.player.display_cards()
+            player_card_3 = pygame.image.load('img/' + self.player.card_img[2] + '.png').convert()
+            gameDisplay.blit(player_card_3, (520, 450))
+
+            if self.player.value > 21:
+                game_finish("You Busted!", 500, 250, red)
+            
+                
+        if self.player_card == 3:
+            self.player.calc_hand()
+            self.player.display_cards()
+            player_card_4 = pygame.image.load('img/' + self.player.card_img[3] + '.png').convert()
+            gameDisplay.blit(player_card_4, (630, 450))
+                
+            if self.player.value > 21:
+                game_finish("You Busted!", 500, 250, red)
+            
+        self.player.value = 0
+
+        if self.player_card > 4:
             sys.exit()
+            
+            
+    def stand(self):
+        show_dealer_card = pygame.image.load('img/' + self.dealer.card_img[1] + '.png').convert()
+        gameDisplay.blit(show_dealer_card, (550, 200))
+        self.blackjack()
+        self.dealer.calc_hand()
+        self.player.calc_hand()
+        if self.player.value > self.dealer.value:
+            game_finish("You Won!", 500, 250, green)
+            #self.run_again()
+        elif self.player.value < self.dealer.value:
+            game_finish("Dealer Wins!", 500, 250, red)
+            #self.run_again()
+        else:
+            game_finish("It's a Tie!", 500, 250, grey)
+            # self.run_again()
+        
+        
+    def exit(self):
+        sys.exit()
+    
+
+
+    # def run_again(self):
+    #     time.sleep(2)
+    #     self.player.value = 0
+    #     self.dealer.value = 0
+    #     gameDisplay.fill(background_color)
+    #     pygame.draw.rect(gameDisplay, grey, pygame.Rect(0, 0, 250, 700))
+    #     pygame.display.update()
+
         
 play_blackjack = Play()
+
 running = True
 
 while running:
